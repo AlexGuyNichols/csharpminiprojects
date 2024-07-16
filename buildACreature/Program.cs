b7c27e78e270d0915bd3eb1bac858dd7f6502da7
alexguynichols <alexguynichols@daxta.co.uk>
2024-07-16 10:24:09 +0100

    chore: Refactor BuildACreature method to support random mode and user-selected mode

--- buildACreature/Program.cs
+++ buildACreature/Program.cs
@@ -6,20 +6,56 @@ namespace ExquisiteCorpse
   {
     static void Main(string[] args)
     {
-      BuildACreature("ghost", "bug", "monster");
+      Console.WriteLine("Build your own creature!");
+      Console.WriteLine("Choose a mode: ");
+      Console.WriteLine("1: Random mode");
+      Console.WriteLine("2: Choose mode");
+      int mode = Convert.ToInt32(Console.ReadLine());
+      if (mode == 1)
+      {
+        BuildACreature();
+      }
+      else if (mode == 2)
+      {
+        Console.WriteLine("Choose a head: ghost, bug, or monster");
+        string head1 = Console.ReadLine();
+        Console.WriteLine("Choose a body: ghost, bug, or monster");
+        string body1 = Console.ReadLine();
+        Console.WriteLine("Choose feet: ghost, bug, or monster");
+        string feet1 = Console.ReadLine();
+        BuildACreature(head1, body1, feet1);
+      }
+      else
+      {
+        Console.WriteLine("Invalid mode");
+      }
     }
 
     static void BuildACreature(string head, string body, string feet)
     {
+      int headNum = TranslateToNumber(head);
+      int bodyNum = TranslateToNumber(body);
+      int feetNum = TranslateToNumber(feet);
+
+      SwitchCase(headNum, bodyNum, feetNum);
+    }
+
+    static void BuildACreature()
+    {
+      RandomMode();
+    }
+
+    static void SwitchCase(int head, int body, int feet)
+    {
       switch (head)
       {
-        case "ghost":
+        case 1:
           GhostHead();
           break;
-        case "bug":
+        case 2:
           BugHead();
           break;
-        case "monster":
+        case 3:
           MonsterHead();
           break;
         default:
@@ -29,13 +65,13 @@ namespace ExquisiteCorpse
 
       switch (body)
       {
-        case "ghost":
+        case 1:
           GhostBody();
           break;
-        case "bug":
+        case 2:
           BugBody();
           break;
-        case "monster":
+        case 3:
           MonsterBody();
           break;
         default:
@@ -45,19 +81,44 @@ namespace ExquisiteCorpse
 
       switch (feet)
       {
-        case "ghost":
+        case 1:
           GhostFeet();
           break;
-        case "bug":
+        case 2:
           BugFeet();
           break;
-        case "monster":
+        case 3:
           MonsterFeet();
           break;
         default:
           Console.WriteLine("Invalid creature part");
           break;
       }
+    }
+
+    static int TranslateToNumber(string creature)
+    {
+      switch (creature)
+      {
+        case "ghost":
+          return 1;
+        case "bug":
+          return 2;
+        case "monster":
+          return 3;
+        default:
+          return 1;
+      }
+    }
+
+    static void RandomMode()
+    {
+      Random randomNumber = new Random();
+      int head = randomNumber.Next(1, 4);
+      int body = randomNumber.Next(1, 4);
+      int feet = randomNumber.Next(1, 4);
+
+      SwitchCase(head, body, feet);
     }
 
     static void GhostHead()
