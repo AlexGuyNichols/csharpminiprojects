097c1f041d7c1d1dff729ba5e08cc0c93c77532f
alexguynichols <alexguynichols@daxta.co.uk>
2024-07-18 12:02:55 +0100

    chore: Update Cesar Cipher program with encryption logic

--- cesarCipher/Program.cs
+++ cesarCipher/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 
-namespace cesarCipher
+namespace CaesarCipher
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+      char[] alphabet = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+      
+      Console.WriteLine("Enter the secret message: ");
+        string input = Console.ReadLine();
+        char[] secretMessage = input.ToCharArray();
+        char[] encryptedMessage = new char[secretMessage.Length];
+        for (int i = 0; i < secretMessage.Length; i++)
         {
-            Console.WriteLine("Hello World!");
+          char letter = secretMessage[i];
+          int letterPosition = Array.IndexOf(alphabet, letter);
+          int newLetterPosition = (letterPosition + 3) % 26;
+          char letterEncoded = alphabet[newLetterPosition];
+          encryptedMessage[i] = letterEncoded;
         }
+
+        string encodedString = String.Join("", encryptedMessage);
+        Console.WriteLine(encodedString);
+
     }
+  }
 }
